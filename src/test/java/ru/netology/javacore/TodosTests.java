f68f94d4c930bf12a2bd5699a0098301c4847518
Sergey <prime-n@mail.ru>
2022-06-07 20:15:58 +0300

    update classes

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,42 @@
 package ru.netology.javacore;
 
-public class TodosTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-    // ваши тесты для класса Todos
+public class TodosTests {
+    Todos todos;
+
+
+    @BeforeEach
+    public void beforeEachMethod() {
+        todos = new Todos();
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"Акробатика", "Пробежка"})
+    public void test_add_task(String e) {
+        todos.addTask(e);
+        Assertions.assertTrue(todos.getAllTasks().contains(e));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Акробатика", "Пробежка"})
+    public void test_remove_task(String g) {
+        todos.addTask(g);
+        todos.removeTask(g);
+        Assertions.assertFalse(todos.getAllTasks().contains(g));
+    }
+
+
+    @Test
+    public void test_get_all_tasks() {
+        todos.addTask("Пробежка");
+        todos.addTask("Акробатика");
+        String expected = "Акробатика Пробежка";
+        String result = todos.getAllTasks();
+        Assertions.assertEquals(result, expected);
+    }
 }
